128dd21347a34411d49fe3cb7ac7f7a2ff40611a
Carlos Matallín <matallo@cartodb.com>
2014-11-01 01:08:58 +0100

    cartodbjs: lesson 1: change jsfiddle wrap

--- t/03-cartodbjs-ground-up/lesson-1/jsfiddle_demo/demo.js
+++ t/03-cartodbjs-ground-up/lesson-1/jsfiddle_demo/demo.js
@@ -1,21 +1,19 @@
-window.addEvent('domready', function() {
-    var vizjson = 'http://documentation.cartodb.com/api/v2/viz/23f2abd6-481b-11e4-8fb1-0e4fddd5de28/viz.json';
+var vizjson = 'http://documentation.cartodb.com/api/v2/viz/23f2abd6-481b-11e4-8fb1-0e4fddd5de28/viz.json';
 
-    // Choose center and zoom level
-    var options = {
-                center: [41.8369, -87.6847], // Chicago
-                zoom: 7
-            }
+// Choose center and zoom level
+var options = {
+    center: [41.8369, -87.6847], // Chicago
+    zoom: 7
+}
 
-    // Instantiate map on specified DOM element
-    var map_object = new L.Map('map', options);
+// Instantiate map on specified DOM element
+var map_object = new L.Map('map', options);
 
-    // Add a basemap to the map object just created
-    L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-        attribution: 'Stamen'
-        }).addTo(map_object);
+// Add a basemap to the map object just created
+L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+    attribution: 'Stamen'
+}).addTo(map_object);
 
-    // Add CartoDB data layers
-    cartodb.createLayer(map_object,vizjson)
-        .addTo(map_object);
-})
+// Add CartoDB data layers
+cartodb.createLayer(map_object,vizjson)
+    .addTo(map_object);
