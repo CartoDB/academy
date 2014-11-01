9aafccbda57cc2a3b8ad2b180ccef22ef7407285
Carlos Matallín <matallo@cartodb.com>
2014-11-01 01:58:28 +0100

    cartodbjs: lesson 1 jsfiddle: onload

--- t/03-cartodbjs-ground-up/lesson-1/jsfiddle_demo/demo.js
+++ t/03-cartodbjs-ground-up/lesson-1/jsfiddle_demo/demo.js
@@ -1,4 +1,4 @@
-$(function () {
+window.onload = function() {
     cdb.init(function() {
         var vizjson = 'http://documentation.cartodb.com/api/v2/viz/23f2abd6-481b-11e4-8fb1-0e4fddd5de28/viz.json';
 
@@ -20,4 +20,4 @@ $(function () {
         cartodb.createLayer(map_object,vizjson)
             .addTo(map_object);
     });
-});
+}
