415fe132fbe9ceb741adaa978af04fd9479db62b
Carlos Matallín <matallo@cartodb.com>
2014-11-01 01:42:12 +0100

    update jsfiddles with wrap: d and cdb.init

--- t/03-cartodbjs-ground-up/lesson-2/jsfiddle_demo_ondone/demo.js
+++ t/03-cartodbjs-ground-up/lesson-2/jsfiddle_demo_ondone/demo.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+cdb.init(function() {
     var vizjson_url = 'http://documentation.cartodb.com/api/v2/viz/ef933ce8-e9ca-11e2-98b0-5404a6a683d5/viz.json';
 
     cartodb.createVis('map', vizjson_url)
@@ -10,4 +10,4 @@ window.onload = function () {
             // report error
             console.log("An error occurred: " + err);
         });
-}
+});
