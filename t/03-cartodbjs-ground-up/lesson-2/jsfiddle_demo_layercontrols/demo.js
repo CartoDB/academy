a1a8e48795f2dc1b4978067422e1cf0154eec7ce
Andy Eschbacher <andy.eschbacher@gmail.com>
2014-11-11 14:39:59 -0500

    Modified OSM attribution

--- t/03-cartodbjs-ground-up/lesson-2/jsfiddle_demo_layercontrols/demo.js
+++ t/03-cartodbjs-ground-up/lesson-2/jsfiddle_demo_layercontrols/demo.js
@@ -26,7 +26,7 @@ window.onload = function() {
 
         // Pull tiles from OpenStreetMap
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: 'OpenStreetMap'
+            attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map_object);
 
         // Add data layer to your map
